630dbcf752bd475d176ac46c77272d6e408a16ff
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-10-28 10:56:59 +0000

    commit new sites

--- src/chapterData.js
+++ src/chapterData.js
@@ -1,156 +1,59 @@
 const chapters = [
   // Initial 20 entries
   {
-    name: "ILO",
-    coordinates: [-99.1332, 19.4326],
+    name: "1: AVOCADO FIELD MICHOACÁN",
+    coordinates: [-100.389908, 19.4326],
     logo: "/logos/ILO.jpg",
     description: "International Labour Organization."
   },
   {
-    name: "APHIS",
-    coordinates: [-100.3557, 20.2345],
+    name: "2: NUEVO LAREDO / LAREDO BORDER ",
+    coordinates: [-99.54490911539774, 27.595396737035994],
     logo: "/logos/APHIS.jpeg",
     description: "Animal and Plant Health Inspection Service."
   },
   {
-    name: "CECOffice",
-    coordinates: [-97.4567, 19.6754],
+    name: "3: UNITED STATES LEGISLATURE ",
+    coordinates: [-77.00901831719571, 38.89008083562472],
     logo: "/logos/cec.jpeg",
     description: "Commission for Environmental Cooperation Office."
   },
   {
-    name: "COFEPRIS",
-    coordinates: [-99.6745, 21.3456],
+    name: "4: MEXICAN LEGISLATURE ",
+    coordinates: [-99.11754939088065, 19.430420630935863],
     logo: "/logos/COFEPRIS.jpg",
     description: "Federal Commission for Protection against Sanitary Risks."
   },
   {
-    name: "CONABIO",
-    coordinates: [-98.5432, 19.9876],
+    name: "5: COMMISSION OF ENVIRONMENT COOPERATION",
+    coordinates: [-73.56440295362928, 45.501020957368596],
+    logo: "/logos/COFEPRIS.jpg",
+    description: "Federal Commission for Protection against Sanitary Risks."
+  },
+  {
+    name: "6: RV FRESH FOODS  ",
+    coordinates: [-102.02889366747085, 19.399041789260906],
     logo: "/logos/CONABIO.jpg",
     description:
       "National Commission for the Knowledge and Use of Biodiversity."
   },
   {
-    name: "CONAFOR",
-    coordinates: [-99.8765, 18.8765],
+    name: "7: AVOCADO EXPORTING PRODUCERS AND PACKERS OF MEXICO ",
+    coordinates: [-102.03742519175485, 19.387245090125127],
     logo: "/logos/CONAFOR.jpg",
     description: "National Forestry Commission of Mexico."
   },
   {
-    name: "CONAGUA",
-    coordinates: [-100.1234, 22.4567],
+    name: "8: LAND USE CHANGE, FOREST AND FIRES ",
+    coordinates: [-101.18170859902112, 19.698126725662217],
     logo: "/logos/CONAGUA.jpeg",
     description: "National Water Commission."
   },
   {
-    name: "CONANP",
-    coordinates: [-102.5678, 20.6789],
+    name: "9: ILLEGAL WATER ",
+    coordinates: [-101.17445496023151, 19.680693050883992],
     logo: "/logos/CONANP.jpeg",
     description: "National Commission of Natural Protected Areas."
-  },
-  {
-    name: "EPA",
-    coordinates: [-77.0369, 38.9072],
-    logo: "/logos/EPA.jpg",
-    description: "Environmental Protection Agency."
-  },
-  {
-    name: "FDA",
-    coordinates: [-77.1234, 38.5678],
-    logo: "/logos/FDA.jpg",
-    description: "Food and Drug Administration."
-  },
-  {
-    name: "IMTA",
-    coordinates: [-99.1415, 18.6294],
-    logo: "/logos/IMTA.jpg",
-    description: "Mexican Institute of Water Technology."
-  },
-  {
-    name: "INECC",
-    coordinates: [-99.1803, 19.3597],
-    logo: "/logos/INECC.jpg",
-    description: "National Institute of Ecology and Climate Change."
-  },
-  {
-    name: "MG",
-    coordinates: [-98.7896, 19.2345],
-    logo: "/logos/MG.jpg",
-    description: "Estados Unidos Mexicanos - Mexican Government."
-  },
-  {
-    name: "MSC",
-    coordinates: [-97.5674, 18.8765],
-    logo: "/logos/MSC.jpg",
-    description: "Supreme Court of Justice of the Nation."
-  },
-  {
-    name: "PROFEPA",
-    coordinates: [-99.5678, 20.4321],
-    logo: "/logos/PROFEPA.jpeg",
-    description: "Federal Attorney for Environmental Protection."
-  },
-  {
-    name: "SENASICA",
-    coordinates: [-100.2345, 19.8765],
-    logo: "/logos/SENASICA.jpg",
-    description:
-      "National Service for Agroalimentary Health, Safety, and Quality."
-  },
-  {
-    name: "SIAP",
-    coordinates: [-99.3456, 18.2345],
-    logo: "/logos/SIAP.jpeg",
-    description: "Agroalimentary and Fishing Information Service."
-  },
-  {
-    name: "USCBP",
-    coordinates: [-77.4567, 38.8765],
-    logo: "/logos/USCBP.jpg",
-    description: "U.S. Customs and Border Protection."
-  },
-  {
-    name: "USDA",
-    coordinates: [-77.2345, 38.5678],
-    logo: "/logos/USDA.jpg",
-    description: "U.S. Department of Agriculture."
-  },
-  {
-    name: "USDC",
-    coordinates: [-77.0369, 38.9072],
-    logo: "/logos/USDC.jpeg",
-    description: "U.S. Department of Commerce."
-  },
-  {
-    name: "USEP",
-    coordinates: [-77.1, 38.905],
-    logo: "/logos/USEP.jpg",
-    description: "Executive Office of the President of the United States."
-  },
-  {
-    name: "USP",
-    coordinates: [-77.101, 38.906],
-    logo: "/logos/USP.jpg",
-    description: "Seal of the President of the United States."
-  },
-  {
-    name: "USPHS",
-    coordinates: [-77.102, 38.907],
-    logo: "/logos/USPHS.jpeg",
-    description: "U.S. Public Health Service."
-  },
-  {
-    name: "WTO",
-    coordinates: [-77.103, 38.908],
-    logo: "/logos/WTO.jpeg",
-    description: "World Trade Organization."
-  },
-  {
-    name: "USDL",
-    coordinates: [-77.103, 38.908],
-    logo: "/logos/USDL.jpeg",
-    description: "XXX."
   }
 ];
 
